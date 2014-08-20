ce25fadd44de61d0e6be0a078b2ef65ae24c2cee
Cool Hand Luke <mrpaulmcdonnell@gmail.com>
2014-08-20 01:19:46 +0100

    Test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Altitude::Application.config.secret_token = '4985b96ab80dd82ba1c244a3573a968d15a372f34e500ab6da9845cfb458355fd272b649f81b8896f797dd0e2f7be8a10263e1b48724988c1db28b8d6ff2fc2f'
